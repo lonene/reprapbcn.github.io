b6eed0c0dcfef0cb3021df932954573741050e9c
kitusmark <electronicslab@icloud.com>
2014-05-27 01:13:39 +0200

    Improved dynamics
    Added some logos, download buttons and a button that appears when
    scrolling down letting going up!.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
 console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+	
+	console.log('ready to work');
+
+//Control del boton de subir arriba
+$(window).scroll(function(){
+	if($(this).scrollTop() > 400){
+		$(".subirArriba").fadeIn();
+	} else {
+		$(".subirArriba").fadeOut();
+	}	
+});
+
+$(".subirArriba").click(function(){
+	$("html, body").animate({scrollTop : 0}, 600);
+	return false;
+	});	
+});
